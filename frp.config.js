7cd4f62de96d8e4fd9595773abe8f6195e886c04
Cisco.K <exiria7@gmail.com>
2018-07-04 18:55:01 +0900

    svg-spriteのサンプルを追加。もろもろ調整など。

--- frp.config.js
+++ frp.config.js
@@ -1,6 +1,7 @@
 'use strict';
 // https://github.com/frontainer/frontplate-cli/wiki/6.%E8%A8%AD%E5%AE%9A
 module.exports = function(production) {
+  global.FRP_TPL_TYPE = 'pug_styl',  // not changed
   global.FRP_SRC = 'src';
   global.FRP_DEST = `${__dirname}/public`;
   return {
